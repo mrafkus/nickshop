09e2e79c2d3f2940cba7cef37c0f6891960c39fa
cowdingus <56494343+cowdingus@users.noreply.github.com>
2022-04-29 22:02:20 +0700

    Fix indentation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,25 @@
 import login from "../assets/login.png";
 
-import Navbar from "../components/Navbar"
-
 export default function Login() {
-    return (
+  return (
+    <div>
+      <div className="flex justify-center">
+        <img src={login} className="items-center h-[420px]" />
+      </div>
+      <div className="flex flex-col items-center m-10">
+        <h3 className="text-xl font-bold text-gray-600">Username</h3>
+        <input
+          className="px-2 text-gray-700 h-7 w-96 ring-2 ring-blue-400 rounded-md grow"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h3 className="text-xl font-bold text-gray-600">Password</h3>
         <div>
-            <Navbar login={false} />
-            <div className="flex justify-center">
-                <img src={login} className="items-center h-[420px]" />
-            </div>
-            <div className="flex flex-col items-center m-10">
-                <h3 className="text-xl font-bold text-gray-600">Username</h3>
-                    <input
-                        className="h-7 w-96 px-2 text-gray-700 ring-2 ring-blue-400 rounded-md grow"
-                    />
-            </div>
-            <div className="flex flex-col items-center">
-                <h3 className="text-xl font-bold text-gray-600">Password</h3>
-                <div>
-                    <input
-                        className="h-7 w-96 px-2 text-gray-700 ring-2 ring-blue-400 rounded-md grow"
-                    />
-                </div>
-            </div>
+          <input
+            className="px-2 text-gray-700 h-7 w-96 ring-2 ring-blue-400 rounded-md grow"
+          />
         </div>
-
-    )
+      </div>
+    </div>
+  )
 }
