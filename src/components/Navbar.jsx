bbac2b51716f5ee9e2133469f7f5d829c81bd432
cowdingus <zaki.nadhif45@gmail.com>
2022-05-26 11:00:19 +0700

    Remove right border in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
 
   return (
     <div className="z-10 h-24 shrink-0">
-      <div className="flex items-center justify-between h-24 bg-white border-r leading-3">
+      <div className="flex items-center justify-between h-24 bg-white leading-3">
         <div className="flex items-center space-x-12 grow-[0.5]">
           <button>
             <a href="/">
